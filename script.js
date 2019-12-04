f0902d00aa0dab17feb27d9d35dfa1f8b7e11324
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2019-12-04 06:13:54 +0200

    Release version 2.0
    Remove extra code in js.
    
    No more semantic errors, validated by validator.
    
    All elements are now adaptive:
      -fixed the height of the "why-best" element;
      -fixed break points for header
      -added break points for correct display in the "comments";
      -changed grid to flex in "footer";
    
    Fixed class names:
      -now everything is according to the "BEM" methodology;
      -each block has its own unique elements;
      -as a result of refactoring it was deleted unnecessary files styles.
    
    Refactoring slides:
      -now it works on the sss;
      -pictures are centered on the screen size.
    
    Refactoring burger-menu:
      -now completely on css;
      -the style of the called menu has been changed;
      -the menu call now works on a screen of any size.

--- script.js
+++ script.js
@@ -1,55 +1,18 @@
-const slide = document.querySelector(".slide");
-const slideChild = slide.children;
-
-let slideNum = 1;
-
-const slideItem = function(num) {
-  for (let i = 0; i < 3; i++) {
-    slideChild[i].classList.remove("sliding");
-  }
-  slideChild[num].classList.add("sliding");
-};
-
-const sliding = function() {
-  setTimeout(() => {
-    slideItem(slideNum);
-    slideNum++;
-
-    if (slideNum < 3) {
-      sliding();
-    } else {
-      slideNum = 0;
-      sliding();
-    }
-  }, 5000);
-};
-
-if (document.documentElement.clientWidth >= 768) {
-  sliding();
-}
-
-const main = document.querySelector(".main");
 const header = document.querySelector(".header");
-const menu = document.querySelector(".menu");
 
 window.addEventListener("scroll", function() {
-  if (this.pageYOffset > header.clientHeight) {
+  if (this.pageYOffset > 20 && this.pageYOffset < 100) {
     header.classList.add("header-fixed");
-    menu.src = "./img/burger-black.svg";
-    menu.style.position = "fixed";
-  } else {
-    header.classList.remove("header-fixed");
-    menu.src = "./img/burger-white.svg";
-    menu.style.position = "absolute";
+    header.style.backgroundColor = `rgb(255, 255, 255, 0.${this.pageYOffset})`;
   }
-});
 
-const headerRight = document.querySelector(".header__right");
+  if (this.pageYOffset >= 100) {
+    header.classList.add("header-fixed");
+    header.style.backgroundColor = "rgb(255, 255, 255, 1)";
+  }
 
-menu.addEventListener("click", function(e) {
-  if (headerRight.classList.contains("menu-active")) {
-    headerRight.classList.remove("menu-active");
-  } else {
-    headerRight.classList.add("menu-active");
+  if (this.pageYOffset < 20) {
+    header.classList.remove("header-fixed");
+    header.style.backgroundColor = "";
   }
 });
